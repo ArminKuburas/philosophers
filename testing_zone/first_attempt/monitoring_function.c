0bc379444f2ff85e0288d8ef21b737e9adf8bc1d
ArminKuburas <akuburass@gmail.com>
2024-03-15 18:08:34 +0200

    Worked on the monitoring func a bit more. Seems functional? Will have to check later

--- testing_zone/first_attempt/monitoring_function.c
+++ testing_zone/first_attempt/monitoring_function.c
@@ -6,7 +6,7 @@
 /*   By: akuburas <akuburas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 14:47:18 by akuburas          #+#    #+#             */
-/*   Updated: 2024/03/15 14:15:20 by akuburas         ###   ########.fr       */
+/*   Updated: 2024/03/15 18:08:05 by akuburas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,9 @@ int	everyone_is_fed(int *thread_fed_check)
 
 void	monitoring(t_pointers *data, int *argv_int)
 {
-	int	i;
-	int	set_to_dead;
+	int				i;
+	int				set_to_dead;
+	struct timeval	current_time;
 
 	set_to_dead = 0;
 	i = 0;
@@ -29,11 +30,17 @@ void	monitoring(t_pointers *data, int *argv_int)
 		if (i == argv_int[0])
 			i = 0;
 		pthread_mutex_lock(&data->monitors[i]);
-		if (data->philo_eat_amount[i] == 0)
-			data->eaten_enough[i] = 1;
-		if (everyone_is_fed(data->eaten_enough) == 1)
+		if (set_to_dead == 1)
 			data->philo_died[i] = 1;
-		if (time_diff(data->philo_wait_start[i], ) >= argv_int[1])
+		else if (data->philo_eat_amount[i] == 0)
+			data->eaten_enough[i] = 1;
+		else if (everyone_is_fed(data->eaten_enough) == 1)
+		{
+			set_to_dead = 1;
+			data->philo_died[i] = 1;
+		}
+		gettimeofday(&current_time, NULL);
+		if (time_diff(data->philo_wait_start[i], current_time) >= argv_int[1])
 		{
 			set_to_dead = 1;
 			data->philo_died[i] = 1;
